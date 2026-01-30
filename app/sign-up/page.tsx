254cabe4c5cfa972fa621be14e96f92ee764a98b
Ittipat <ittipat145@gmail.com>
2026-01-30 22:18:06 +0700

    ✨ feat: update background colors for sign-in and sign-up pages, and adjust logo size in signup form

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 export default function SignUpPage() {
     return (
         <Suspense>
-            <main className="min-h-screen bg-linear-to-br from-neutral-950 via-neutral-900 to-neutral-950 px-4 py-12 flex items-center justify-center">
+            <main className="min-h-screen bg-linear-to-br from-neutral-300 via-neutral-100 to-neutral-300 dark:from-neutral-950 dark:via-neutral-900 dark:to-neutral-950 px-4 py-12 flex items-center justify-center">
                 <SignupForm />
             </main>
         </Suspense>
